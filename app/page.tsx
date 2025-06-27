9b0ae04e10cbfa10cd6ab22c6e5b6d42ebe9cf78
unknown <jkturner107@gmail.com>
2025-06-27 07:22:32 +0700

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,83 @@
-import Image from "next/image";
+'use client'
+import { useState } from "react";
+
+const fruits = [
+  "Apple",
+  "Banana",
+  "Orange",
+  "Grape",
+  "Strawberry",
+  "Watermelon",
+  "Pineapple",
+  "Mango",
+  "Kiwi",
+  "Cherry",
+  "Peach",
+  "Pear",
+  "Plum",
+  "Lemon",
+  "Lime",
+  "Raspberry",
+  "Blueberry",
+  "Blackberry",
+  "Pomegranate",
+  "Fig",
+  "Apricot",
+  "Cantaloupe",
+  "Dragon fruit",
+  "Passion fruit"
+]
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+	const [currentPage, setCurrentPage] = useState(1);
+	const itemsPerPage = 4;
+	const totalPages = Math.ceil(fruits.length / itemsPerPage);
+	const startingIndex = (currentPage - 1) * itemsPerPage; // first page starts at index 0, second age starts at index 4 etc...
+	const currentShowing = fruits.slice(startingIndex, startingIndex + itemsPerPage); // from starting index + 4 will be the fruits we want to display
+
+	const goToNext = () => setCurrentPage(prev => Math.min(prev + 1, totalPages));
+	const goToPrev = () => setCurrentPage(prev => Math.max(prev -1, 1));
+
+	return (
+		<div className="flex flex-col gap-4 p-8">
+			<h1 className="font-semibold text-lg">Pagination Tutorial</h1>
+			{currentShowing && (
+				<div className="flex gap-4">
+					{currentShowing.map((fruit) => (
+						<span 
+							key={fruit}
+							className="bg-slate-600 py-2 px-4 rounded-md"
+							>{fruit}</span>
+					))}
+				</div>
+			)}
+			{fruits.length > 0 && (
+				<div className="flex items-center gap-4">
+					<button
+						onClick={goToPrev}
+						disabled={currentPage === 1}
+						className="bg-sky-600 text-sm py-2 px-4 cursor-pointer rounded-md disabled:opacity-50"
+						>Prev</button>
+					<span>Page {currentPage} of {totalPages}</span>
+					<button
+						onClick={goToNext}
+						disabled={currentPage === totalPages}
+						className="bg-sky-600 text-sm py-2 px-4 cursor-pointer rounded-md disabled:opacity-50"
+						>Next</button>
+					<div className="flex gap-2 items-center">
+						Go to page: 
+						<input 
+							type="number" 
+							value={currentPage} 
+							onChange={e => setCurrentPage(Number(e.target.value))}
+							min={1}
+							max={totalPages}
+							className="bg-slate-400 text-slate-800 p-2 "
+							/>
+					</div>
+				</div>
+			)}
+		</div>
+	);
 }
